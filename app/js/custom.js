9c8ca663eb4a0f2005a2c59c976f5dba2ed9cd84
alex <tochcha@gmail.com>
2019-05-23 13:26:15 +0300

    Первая страница

--- app/js/custom.js
+++ app/js/custom.js
@@ -84,17 +84,65 @@ $(".uni-overlay").click(function() {
 });*/
 
 /* tabs*/
-/*$('.tabs li a').click(function () {
+$('.tabs li a').click(function () {
 	$(this).parents('.tab-wrap').find('.tab-cont').addClass('hide-tab');
 	$(this).parent().siblings().removeClass('active');
 	var id = $(this).attr('href');
 	$(id).removeClass('hide-tab');
 	$(this).parent().addClass('active');
+    if ( $(".nav_tab_1").hasClass("active") ) {
+        $('.nav-tab-list').removeClass('act_2');
+        $('.nav-tab-list').removeClass('act_3');
+        $('.nav-tab-list').addClass('act_1');
+    }
+    if ( $(".nav_tab_2").hasClass("active") ) {
+        $('.nav-tab-list').removeClass('act_3');
+        $('.nav-tab-list').removeClass('act_1');
+        $('.nav-tab-list').addClass('act_2');
+    }
+    if ( $(".nav_tab_3").hasClass("active") ) {
+        $('.nav-tab-list').removeClass('act_2');
+        $('.nav-tab-list').removeClass('act_1');
+        $('.nav-tab-list').addClass('act_3');
+    }
 	return false;
-});*/
+});
 /* tabs*/
 
 // Если нужно при адаптации вырезать блок и вставит в другое место 
-/*if ($(window).width() < 768) {
-	$('.crop-block').detach().insertAfter($('.after-block'));
-}*/
+if ($(window).width() < 600) {
+	$('#tab_1').detach().insertAfter($('.nav_tab_1 a'));
+    $('#tab_2').detach().insertAfter($('.nav_tab_2 a'));
+    $('#tab_3').detach().insertAfter($('.nav_tab_3 a'));
+}
+
+
+// слайдер (настройки тут http://kenwheeler.github.io/slick/)
+if ($('.reviews_slider').length) {
+    $('.reviews_slider').slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<div class="revarrow slick-prev"></div>',
+        nextArrow: '<div class="revarrow slick-next"></div>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+};
